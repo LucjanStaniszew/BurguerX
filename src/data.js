e3888597099c49d431d0647d1a60121095e6c7ec
LucjanStaniszew <luciano.stani97@gmail.com>
2023-01-19 20:51:34 -0300

    Menu de todas las burguers, sandwiches y fritas que hay

---
+++ src/data.js
@@ -0,0 +1,91 @@
+export const menu = [
+    // Hamburguesas
+    {
+        id: 1,
+        name: "Hamburguesa Simple",
+        desc: "La clasica hamburguesa mundial. Pan, Medallón de carne vacuna, lechuga y Tomate",
+        img: "./assets/hamburguesa-simple.jpg",
+        precio: 900,
+    },
+
+    {
+        id: 2,
+        name: "Hamburguesa Doble",
+        desc: "Hamburguesa Doble Carne, Queso Cheddar, Bacon y Huevo Frito",
+        img: "./assets/hamburguesa-doble.jpg",
+        precio: 1200,
+    },
+
+    {
+        id: 3,
+        name: "Hamburguesa Triple",
+        desc: "Hamburguesa Triple Carne, Doble Cheddar, Bacon, Huevo Frito, Lechuga, Tomate y Salsa BurguerX",
+        img: "./assets/hamburguesa-triple.jpg",
+        precio: 1500,
+    },
+
+    {
+        id: 4,
+        name: "Hamburguesa Doble Cheddar, Doble Bacon",
+        desc: "Hamburguesa Doble Carne, Doble Cheddar, Doble Bacon, Salsa BurguerX y Huevo Frito",
+        img: "./assets/doble-b-doble-c.jpg",
+        precio: 1800,
+    },
+
+    {
+        id: 5,
+        name: "BurguerX",
+        desc: "Quadruple hamburguesa, Extra Cheddar, Extra Bacon, Lechuga, Tomate, Pepinos y Salsa BurguerX",
+        img: "./assets/hamburguesa-burguerx.jpg",
+        precio: 1800,
+    },
+    // Sandwiches
+    {
+        id: 6,
+        name: "Sandwich de Milanesa Simple",
+        desc: "Clasico Sandwich de Milanesa, Pan, Milanesa de Ternera, Lechuga y Tomate",
+        img: "./assets/sandwich-mila-simple.jpg",
+        precio: 800,
+    },
+
+    {
+        id: 7,
+        name: "Sandwich de Milanesa Completo",
+        desc: "Sandwich de Milanesa Completo, Pan, Milanesa de Ternera, Jamón, Queso, Lechuga y Tomate",
+        img: "./assets/sandwich-mila-completo.jpg",
+        precio: 1100,
+    },
+
+    {
+        id: 8,
+        name: "Sandwich de Milanesa Napolitano",
+        desc: "Sandwich Napolitano, Doble Milanesa de Ternera, Salsa BurguerX, Mozzarella, Tomate",
+        img: "./assets/sandwich-mila-napolitano.jpg",
+        precio: 1500,
+    },
+    // Fritas
+    {
+        id: 9,
+        name: "Papas Fritas con Cheddar",
+        desc: "Clasicas Papas Fritas con extra queso Cheddar Fundido por encima",
+        img: "./assets/fritas-c-ch.jpg",
+        precio: 1000,
+    },
+    {
+        id: 10,
+        name: "Papas Fritas con Cheddar y Bacon",
+        desc: "Papas Fritas con extra queso Cheddar Fundido y Bacon Frito",
+        img: "./assets/fritas-c-ch-p.jpg",
+        precio: 1400,
+    },
+    {
+        id: 11,
+        name: "Fritas BurguerX",
+        desc: "Papas Fritas con extra queso Cheddar Fundido, Bacon Frito y Perejil Triturado",
+        img: "./assets/fritas-ch-t-p.jpg",
+        precio: 1600,
+    },
+
+]
+
+export default menu;
