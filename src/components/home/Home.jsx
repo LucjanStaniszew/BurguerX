244b96d56d60a1153056596eb8fc0dda1c4d7ede
LucjanStaniszew <luciano.stani97@gmail.com>
2023-01-19 17:54:03 -0300

    New commit, Created BurguerX main Files, Added some Assets

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../navbar/Navbar.jsx';
+
+const home = () => {
+  return (
+    <div>
+        <Navbar />
+        <main>
+        
+        </main>
+    </div>
+  )
+}
+
+export default home
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import BurguerX from '../../assets/BurguerX.webp'
+
+const navbar = () => {
+  return (
+    <div>
+        <header>
+            <nav>
+              <a href='/'><img src="./assets/BurguerX.png" alt='BurgerX Logo' /></a>
+              <a href='/'>Burgerx</a>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default navbar
