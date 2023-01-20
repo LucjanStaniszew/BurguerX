0cb34eae2b1509e99ece6baf63a62e8e443f1c59
LucjanStaniszew <luciano.stani97@gmail.com>
2023-01-19 21:22:18 -0300

    agregado el componente Carrusel al home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 import Navbar from '../navbar/NavBar.jsx';
 import Footer from '../footer/Footer.jsx';
-import './Home.css'
+import CarruselDeComida from '../carrusel/CarruselDeComida.jsx';
+import './Home.css';
 
-const home = () => {
+// import { useState } from 'react';
+// import menu from '../../data.js'
+
+const Home = () => {
+
+  // Carrusel de comida
+  // const [currentIndex, setCurrentIndex] = useState(0);
+
+  // const next = () => {
+  //   setCurrentIndex((currentIndex + 1) % menu.length)
+  // }
+
+  // const prev = () => {
+  //   setCurrentIndex((currentIndex - 1 + menu.length) % menu.length)
+  // }
+
   return (
     <div>
         <Navbar />
 
         <main className='main'>
-        Main
+          
+          <div className='slider'>
+            
+            <CarruselDeComida />
+            
+          </div>
+
         </main>
 
         <Footer />
@@ -17,4 +39,4 @@ const home = () => {
   )
 }
 
-export default home
+export default Home
